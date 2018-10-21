b4a75617dc02c9d7077c25e5055d4460590e1d18
FrankSun <franksun1996@gmail.com>
2018-10-21 16:06:53 +0100

    adding back-end databases firebase for few functions (#4)

--- todo-vue/src/components/TodoList.vue
+++ todo-vue/src/components/TodoList.vue
@@ -40,23 +40,11 @@
       return {
         newTodo: '',
         idForTodo: 3,
-        beforeEditCache: '',
-        filter: 'all',
-        todos: [
-          {
-            'id': 1,
-            'title': 'Finish Vue Screencast',
-            'completed': false,
-            'editing': false,
-          },
-          {
-            'id': 2,
-            'title': 'Take over world',
-            'completed': false,
-            'editing': false,
-          },
-        ]
+        beforeEditCache: ''
       }
+    },
+    created() {
+      this.$store.dispatch('retrieveTodos')
     },
     computed: {
       remaining() {
